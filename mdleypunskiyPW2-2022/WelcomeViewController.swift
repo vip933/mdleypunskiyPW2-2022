c1f7d17fa13277dbe39502eaf7b6d648a0dd55b9
vip933 <mdleypunskiy@edu.hse.ru>
2022-09-17 13:23:22 +0300

    Label and button added

--- mdleypunskiyPW2-2022/WelcomeViewController.swift
+++ mdleypunskiyPW2-2022/WelcomeViewController.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 final class WelcomeViewController: UIViewController {
+    
+    private let commentLabel = UILabel()
+    private let valueLabel = UILabel()
+    private let value: Int = 0
+    private let incrementButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +22,31 @@ final class WelcomeViewController: UIViewController {
 
     private func setupUI() {
         view.backgroundColor = .lightGray
+        setupIncrementButton()
+        setupValueLabel()
+    }
+    
+    private func setupIncrementButton() {
+        incrementButton.setTitle("Increment", for: .normal)
+        incrementButton.setTitleColor(.black, for: .normal)
+        incrementButton.layer.cornerRadius = 12
+        incrementButton.titleLabel?.font = .systemFont(ofSize: 16.0, weight: .medium)
+        incrementButton.backgroundColor = .white
+        //incrementButton.layer.applyShadow()
+        view.addSubview(incrementButton)
+        incrementButton.setHeight(to: 48)
+        incrementButton.pinTop(to: view.centerYAnchor)
+        incrementButton.pin(to: view, [.left: 24, .right: 24])
+        //incrementButton.addTarget(self, action: #selector(incrementButtonPressed), for .touchUpInside)
+    }
+                                        
+    private func setupValueLabel() {
+        valueLabel.font = .systemFont(ofSize: 40.0, weight: .bold)
+        valueLabel.textColor = .black
+        valueLabel.text = "\(value)"
+        view.addSubview(valueLabel)
+        valueLabel.pinBottom(to: incrementButton.topAnchor, 16)
+        valueLabel.pinCenter(to: view.centerXAnchor)
     }
 }
 
