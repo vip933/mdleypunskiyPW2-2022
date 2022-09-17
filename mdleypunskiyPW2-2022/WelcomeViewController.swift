b0d9478a6641d49f04785db62da110e03b01c0ad
vip933 <mdleypunskiy@edu.hse.ru>
2022-09-17 13:32:34 +0300

    Label value now is connected to button's push action

--- mdleypunskiyPW2-2022/WelcomeViewController.swift
+++ mdleypunskiyPW2-2022/WelcomeViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 final class WelcomeViewController: UIViewController {
     
+    private let INC_BUTTON_CORNER_RADIUS: CGFloat = 12
+    private let INC_BUTTON_FONT_SIZE: CGFloat = 25
+    private let INC_BUTTON_TEXT = "Push me"
+    
+    private let VALUE_LABEL_FONT_SIZE: CGFloat = 40
+    
     private let commentLabel = UILabel()
     private let valueLabel = UILabel()
-    private let value: Int = 0
+    private var value: Int = 0
     private let incrementButton = UIButton()
 
     override func viewDidLoad() {
@@ -27,26 +33,36 @@ final class WelcomeViewController: UIViewController {
     }
     
     private func setupIncrementButton() {
-        incrementButton.setTitle("Increment", for: .normal)
+        incrementButton.setTitle(INC_BUTTON_TEXT, for: .normal)
         incrementButton.setTitleColor(.black, for: .normal)
-        incrementButton.layer.cornerRadius = 12
-        incrementButton.titleLabel?.font = .systemFont(ofSize: 16.0, weight: .medium)
+        incrementButton.layer.cornerRadius = INC_BUTTON_CORNER_RADIUS
+        incrementButton.titleLabel?.font = .systemFont(ofSize: INC_BUTTON_FONT_SIZE, weight: .medium)
         incrementButton.backgroundColor = .white
         //incrementButton.layer.applyShadow()
         view.addSubview(incrementButton)
         incrementButton.setHeight(to: 48)
         incrementButton.pinTop(to: view.centerYAnchor)
         incrementButton.pin(to: view, [.left: 24, .right: 24])
-        //incrementButton.addTarget(self, action: #selector(incrementButtonPressed), for .touchUpInside)
+        incrementButton.addTarget(self, action: #selector(incrementButtonPressed), for: .touchUpInside)
     }
                                         
     private func setupValueLabel() {
-        valueLabel.font = .systemFont(ofSize: 40.0, weight: .bold)
+        valueLabel.font = .systemFont(ofSize: VALUE_LABEL_FONT_SIZE, weight: .bold)
         valueLabel.textColor = .black
         valueLabel.text = "\(value)"
         view.addSubview(valueLabel)
         valueLabel.pinBottom(to: incrementButton.topAnchor, 16)
         valueLabel.pinCenter(to: view.centerXAnchor)
+    }
+    
+    @objc
+    private func incrementButtonPressed() {
+        value += 1
+        updateUI()
+    }
+    
+    private func updateUI() {
+        valueLabel.text = "\(value)"
     }
 }
 
