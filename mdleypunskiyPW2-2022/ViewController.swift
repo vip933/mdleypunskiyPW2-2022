536a126ffa8dc0104c336bff6315316b307a34b7
vip933 <mdleypunskiy@edu.hse.ru>
2022-09-17 12:50:56 +0300

    Initial Commit

---
+++ mdleypunskiyPW2-2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mdleypunskiyPW2-2022
+//
+//  Created by Maxim Leypunskiy on 17/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
